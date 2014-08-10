4eed730e1f7f685cd1d0e6efdd82a53b8bdd0e4b
Martin Kollaard <martin@kollaard.eu>
2014-08-11 00:28:59 +0200

    build.sh fix for newer versions of gcc

--- p2000scanner/build.sh
+++ p2000scanner/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Building scanner";
-gcc -Wall -lm -std=c99 -l json -lzmq -pthread -I/usr/include/json/ -o scanner scanner.c
+gcc -std=c99 -Wall -lm -o scanner scanner.c -ljson -lzmq -pthread -I/usr/include/json/
 
 exit 1;
